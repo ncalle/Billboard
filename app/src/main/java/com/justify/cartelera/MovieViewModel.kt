1c4a580fda52e09725809ba7a820a762d2d5d60c
Natalia Calle <nacalle@gmail.com>
2023-12-22 21:28:13 -0300

    sorting list by release date

--- app/src/main/java/com/justify/cartelera/MovieViewModel.kt
+++ app/src/main/java/com/justify/cartelera/MovieViewModel.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.viewModelScope
 import com.justify.cartelera.api.ApiService
 import com.justify.cartelera.api.ServiceGenerator
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MovieViewModel : ViewModel() {
 
@@ -22,11 +24,67 @@ class MovieViewModel : ViewModel() {
                 val serviceGenerator = ServiceGenerator.buildService(ApiService::class.java)
                 val call = serviceGenerator.getMovies(key = keyValue)
                 Log.d("request", call.toString())
-                _movies.value = call.items.sortedByDescending { it.releaseState } // call.items
+
+                var sortMovies = call.items.sortedWith(compareByDescending<MovieModel> { getYear(it.releaseState) }
+                    .thenByDescending { getMonth(it.releaseState) }
+                    .thenByDescending { getDay(it.releaseState) })
+                Log.d("sortMovies", sortMovies.toString())
+                _movies.value = sortMovies
 
             } catch (e: Exception) {
                 Log.e("MovieViewModel", "Error: ${e.message}")
             }
         }
+    }
+
+    private fun getDay(dateString: String): Int? {
+        try {
+            val format = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+            val date = format.parse(dateString)
+
+            if (date != null) {
+                val calendar = java.util.Calendar.getInstance()
+                calendar.time = date
+                return calendar.get(java.util.Calendar.DAY_OF_MONTH)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+        return null
+    }
+
+    private fun getMonth(dateString: String): Int? {
+        try {
+            val format = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+            val date = format.parse(dateString)
+
+            if (date != null) {
+                val calendar = java.util.Calendar.getInstance()
+                calendar.time = date
+                return calendar.get(java.util.Calendar.MONTH) + 1
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+        return null
+    }
+
+    private fun getYear(dateString: String): Int? {
+        try {
+            val format = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+            val date = format.parse(dateString)
+
+            if (date != null) {
+                val calendar = java.util.Calendar.getInstance()
+                calendar.time = date
+                return calendar.get(java.util.Calendar.YEAR)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+        return null
     }
 }
